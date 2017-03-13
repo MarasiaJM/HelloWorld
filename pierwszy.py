394da9c0abf35ab510eb69290018b057a6c9ad74
MarasiaJM <kalifat@wp.pl>
2017-03-13 22:33:59 +0100

    Add files via upload

---
+++ pierwszy.py
@@ -0,0 +1,7 @@
+# -*- coding: cp1250 -*-
+print 'Sprawdzam, czy liczba jest ca³kowita.'
+liczba=raw_input("Podaj liczbê:")
+if float(liczba)==int(liczba):
+    print "Liczba ca³kowita"
+else:
+    print "Liczba u³amkowa"
